d7d5eabc53360c34607c6d86b0843ccf8c2910f3
Dr_Newbie <tf2baidonation@gmail.com>
2017-12-25 03:07:48 +0800

    .

--- Ace of Aces/Lua/Hack/second_chances/missiondoor.lua
+++ Ace of Aces/Lua/Hack/second_chances/missiondoor.lua
@@ -1,13 +1,8 @@
 Hooks:PostHook(MissionDoor, "activate", 'AA_MissionDoor_activate', function(self)
 	if self._unit and alive(self._unit) and self._unit:interaction() then
-		if type(self._devices) == "table" then
-			for _type, data in pairs(self._devices) do
-				if _type == "key" then
-					self._unit:interaction():set_tweak_data("hack_keycard")
-					self._unit:interaction():set_active(true, false)
-					break
-				end
-			end
+		if type(self._devices) == "table" and self._devices.key then
+			self._unit:interaction():set_tweak_data("hack_keycard")
+			self._unit:interaction():set_active(true, false)
 		end
 	end
 end)
