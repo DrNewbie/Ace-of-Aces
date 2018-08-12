3ff24e08b02a3bc9aa99f2407d51d443a6ba4b22
Dr_Newbie <tf2baidonation@gmail.com>
2018-08-12 09:36:05 +0800

    55 ; pick_lock_so_hard bugs

--- Ace of Aces/Lua/Hack/second_chances/interactionext.lua
+++ Ace of Aces/Lua/Hack/second_chances/interactionext.lua
@@ -3,7 +3,9 @@ local SpecialHackLock_interact_start = UseInteractionExt.interact_start
 local SpecialHackLock_post_event = UseInteractionExt._post_event
 local SpecialHackLock_selected = UseInteractionExt.selected
 local SpecialHackLock_can_select = UseInteractionExt.can_select
-local SpecialHackLock_SkillAA = function()
+local SpecialHackLock_can_interact = UseInteractionExt.can_interact
+
+function SpecialHackLock_SkillAA()
 	return managers.player:has_category_upgrade("player", "pick_lock_so_hard")
 end
 
@@ -25,7 +27,7 @@ function UseInteractionExt:can_hack_keycard()
 	if SpecialHackLock_SkillAA() then
 		return SpecialHackLock_CheckList(self.tweak_data)
 	end
-	return true
+	return false
 end
 
 function UseInteractionExt:can_select(...)
@@ -36,43 +38,36 @@ function UseInteractionExt:can_select(...)
 end
 
 function UseInteractionExt:_timer_value()
-	if SpecialHackLock_SkillAA() then
-		if SpecialHackLock_ChancgeList(self.tweak_data) then
-			return tweak_data.interaction.hack_keycard.timer or 0
-		end
+	if self:can_hack_keycard() then
+		return tweak_data.interaction.hack_keycard.timer or 0
 	end
 	return self._tweak_data.timer or 0
 end
 
 function UseInteractionExt:interact_start(player)
-	if SpecialHackLock_SkillAA() then
-		if SpecialHackLock_ChancgeList(self.tweak_data) and self.tweak_data ~= "hack_keycard" then
-			local blocked, skip_hint, custom_hint = self:_interact_blocked(player)
-			local tweak_timer = tweak_data.interaction.hack_keycard.timer or 0
-			local has_equipment = not self._tweak_data.special_equipment and true or managers.player:has_special_equipment(self._tweak_data.special_equipment)
-			if blocked then
-				if not skip_hint and (custom_hint or self._tweak_data.blocked_hint) then
-					managers.hint:show_hint(custom_hint or self._tweak_data.blocked_hint)
-				end
-
-				return false
+	if self:can_hack_keycard() and self.tweak_data ~= "hack_keycard" then
+		local blocked, skip_hint, custom_hint = self:_interact_blocked(player)
+		local tweak_timer = tweak_data.interaction.hack_keycard.timer or 0
+		local has_equipment = not self._tweak_data.special_equipment and true or managers.player:has_special_equipment(self._tweak_data.special_equipment)
+		if blocked then
+			if not skip_hint and (custom_hint or self._tweak_data.blocked_hint) then
+				managers.hint:show_hint(custom_hint or self._tweak_data.blocked_hint)
 			end
-			if tweak_timer > 0 and not has_equipment then
-				if not self:can_interact(player) then
-					if self._tweak_data.blocked_hint then
-						managers.hint:show_hint(self._tweak_data.blocked_hint)
-					end
-
+			return false
+		end
+		if tweak_timer > 0 and not has_equipment then
+			if not self:can_interact(player) then
+				if self._tweak_data.blocked_hint then
+					managers.hint:show_hint(self._tweak_data.blocked_hint)
+				end
 					return false
-				end
-				local run_time = self:_get_timer()
-				if run_time > 0 then
-					self:_at_interact_start(player, run_time)
-					self._tweak_data_at_interact_start = tweak_data.interaction.hack_keycard
-					return false, run_time
-				end
 			end
-			return self:interact(player)
+			local run_time = self:_get_timer()
+			if run_time > 0 then
+				self:_at_interact_start(player, run_time)
+				self._tweak_data_at_interact_start = tweak_data.interaction.hack_keycard
+				return false, run_time
+			end
 		end
 	end
 	return SpecialHackLock_interact_start(self, player)
@@ -80,15 +75,13 @@ end
 
 function UseInteractionExt:selected(...)
 	local Ans = SpecialHackLock_selected(self, ...)
-	if SpecialHackLock_SkillAA() then
-		if SpecialHackLock_ChancgeList(self.tweak_data) then
-			local has_equipment = not self._tweak_data.special_equipment and true or managers.player:has_special_equipment(self._tweak_data.special_equipment)
-			if not has_equipment then
-				managers.hud:show_interact({
-					text = managers.localization:text(tweak_data.interaction.hack_keycard.text_id),
-					icon = self.no_equipment_icon or self._tweak_data.no_equipment_icon or self._tweak_data.icon
-				})
-			end
+	if self:can_hack_keycard() then
+		local has_equipment = not self._tweak_data.special_equipment and true or managers.player:has_special_equipment(self._tweak_data.special_equipment)
+		if not has_equipment then
+			managers.hud:show_interact({
+				text = managers.localization:text(tweak_data.interaction.hack_keycard.text_id),
+				icon = self.no_equipment_icon or self._tweak_data.no_equipment_icon or self._tweak_data.icon
+			})
 		end
 	end
 	return Ans
@@ -102,13 +95,13 @@ function UseInteractionExt:_post_event(...)
 end
 
 Hooks:PostHook(UseInteractionExt, "_at_interact_start", 'SpecialHackLock_at_interact_start', function(self, player)
-	if self.tweak_data == "hack_keycard" then
+	if self:can_hack_keycard() then
 		player:base():set_detection_multiplier("hack_keycard", 0.5)
 	end
 end)
 
 Hooks:PostHook(UseInteractionExt, "_at_interact_interupt", 'SpecialHackLock_interact_interupt', function(self, player)
-	if self.tweak_data == "hack_keycard" then
+	if self:can_hack_keycard() then
 		player:base():set_detection_multiplier("hack_keycard", 1)
 	end
 end)
@@ -143,26 +136,34 @@ function UseInteractionExt:can_interact(player)
 	if self._host_only and not Network:is_server() then
 		return false
 	end
+
 	if self._disabled then
 		return false
 	end
+
 	if not self:_has_required_upgrade(alive(player) and player:movement() and player:movement().current_state_name and player:movement():current_state_name()) then
 		return false
 	end
+
 	if not self:_has_required_deployable() then
 		return false
 	end
+
 	if not self:_is_in_required_state(alive(player) and player:movement() and player:movement().current_state_name and player:movement():current_state_name()) then
 		return false
 	end
+
 	if self._tweak_data.special_equipment_block and managers.player:has_special_equipment(self._tweak_data.special_equipment_block) then
 		return false
 	end
+
 	if not self._tweak_data.special_equipment or self._tweak_data.dont_need_equipment then
 		return true
 	end
-	if SpecialHackLock_SkillAA() and SpecialHackLock_CheckList(self.tweak_data) then
+	
+	if self:can_hack_keycard() then
 		return true
 	end
+
 	return managers.player:has_special_equipment(self._tweak_data.special_equipment)
 end
--- Ace of Aces/Lua/Hack/second_chances/missiondoor.lua
+++ Ace of Aces/Lua/Hack/second_chances/missiondoor.lua
@@ -1,8 +1,1 @@
-Hooks:PostHook(MissionDoor, "activate", 'AA_MissionDoor_activate', function(self)
-	if self._unit and alive(self._unit) and self._unit:interaction() then
-		if type(self._devices) == "table" and self._devices.key then
-			self._unit:interaction():set_tweak_data("hack_keycard")
-			self._unit:interaction():set_active(true, false)
-		end
-	end
-end)
+--??
