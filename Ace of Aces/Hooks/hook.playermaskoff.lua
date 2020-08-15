cfcb02a06aa179e78cfaa6ca0f053a637522f2b2
Dr_Newbie <tf2baidonation@gmail.com>
2020-08-15 22:06:10 +0800

    82.1 ; jump and duck

--- Ace of Aces/Hooks/hook.playermaskoff.lua
+++ Ace of Aces/Hooks/hook.playermaskoff.lua
@@ -1,66 +1,27 @@
-local AA_MaskOff_check_action_jump = PlayerMaskOff._check_action_jump
-local AA_MaskOff_check_action_duck = PlayerMaskOff._check_action_duck
+local mod_ids = Idstring('AA:Jump and Duck in Casing Mode'):key()
 
-function PlayerMaskOff:_check_action_jump(t, input)
-	if input.btn_jump_press then
-		if managers.player:has_category_upgrade("player", "mask_off_movement") then
-			PlayerStandard._check_action_jump(self, t, input)
-			return
-		end
-		AA_MaskOff_check_action_jump(self, t, input)
+Hooks:PostHook(PlayerMaskOff, "_check_action_jump", 'F_'..Idstring("PostHook:PlayerMaskOff:_check_action_jump:"..mod_ids):key(), function(self, t, input)
+	if managers.player:has_category_upgrade("player", "mask_off_movement") and input.btn_jump_press then
+		PlayerStandard._check_action_jump(self, t, input)
 	end
-end
+end)
 
-function PlayerMaskOff:_check_action_duck(t, input)
-	if input.btn_duck_press then
-		if managers.player:has_category_upgrade("player", "mask_off_movement") then
-			if not self._state_data.ducking then
-				PlayerStandard._start_action_ducking(self, t)
-			elseif self._state_data.ducking then
-				PlayerStandard._end_action_ducking(self, t)
-			end
-			return
-		end
-		AA_MaskOff_check_action_duck(self, t, input)
+Hooks:PostHook(PlayerMaskOff, "_check_action_duck", 'F_'..Idstring("PostHook:PlayerMaskOff:_check_action_duck:"..mod_ids):key(), function(self, t, input)
+	if managers.player:has_category_upgrade("player", "mask_off_movement") and (input.btn_duck_release or input.btn_duck_press) then
+		PlayerStandard._check_action_duck(self, t, input)
 	end
-end
+end)
 
-local AceAces_MaskOff = PlayerMaskOff._update_check_actions
-
-function PlayerMaskOff:_update_check_actions(t, dt)
-	if not managers.player:has_category_upgrade("player", "mask_off_movement") then
-		AceAces_MaskOff(self, t, dt)
+Hooks:PostHook(PlayerMaskOff, "_check_action_interact", 'F_'..Idstring("PostHook:PlayerMaskOff:_check_action_interact:"..mod_ids):key(), function(self, ...)
+	if managers.player:has_category_upgrade("player", "mask_off_movement") then 
+		return true
 	else
-		local input = self:_get_input(t, dt)
-		self._stick_move = self._controller:get_input_axis("move")
-
-		if mvector3.length(self._stick_move) < 0.1 or self:_interacting() then
-			self._move_dir = nil
-		else
-			self._move_dir = mvector3.copy(self._stick_move)
-			local cam_flat_rot = Rotation(self._cam_fwd_flat, math.UP)
-
-			mvector3.rotate_with(self._move_dir, cam_flat_rot)
-		end
-
-		self:_update_interaction_timers(t)
-		self:_update_start_standard_timers(t)
-
-		if input.btn_stats_screen_press then
-			self._unit:base():set_stats_screen_visible(true)
-		elseif input.btn_stats_screen_release then
-			self._unit:base():set_stats_screen_visible(false)
-		end
-
-		self:_update_foley(t, input)
-
-		local new_action = nil
-		new_action = new_action or self:_check_use_item(t, input)
-		new_action = new_action or self:_check_action_interact(t, input)
-
-		self:_check_action_jump(t, input)
-		self:_check_action_duck(t, input)
-		self:_check_action_run(t, input)
-		self:_check_action_change_equipment(t, input)
+		return PlayerMaskOff.super._check_action_interact(self, ...)
 	end
-end
+end)
+
+Hooks:PostHook(PlayerMaskOff, "_upd_attention", 'F_'..Idstring("PostHook:PlayerMaskOff:_upd_attention:"..mod_ids):key(), function(self)
+	if managers.player:has_category_upgrade("player", "mask_off_movement") and self._state_data.ducking then
+		PlayerStandard._upd_attention(self)
+	end
+end)
