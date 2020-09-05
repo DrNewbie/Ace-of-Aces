783b4fcf5dddc79a22c3e184fec1f23b2725af67
Dr_Newbie <tf2baidonation@gmail.com>
2020-09-05 12:11:53 +0800

    83.5 ; Inspire AA distance

--- Ace of Aces/Hooks/hook.playermovement.lua
+++ Ace of Aces/Hooks/hook.playermovement.lua
@@ -1,6 +1,6 @@
 Hooks:PostHook(PlayerMovement, "init", "AA_PlayerMovementInit", function(self)
 	if managers.player:has_category_upgrade("player", "far_long_dis_revive") and self._rally_skill_data and self._rally_skill_data.range_sq then
-		self._rally_skill_data.range_sq = 810000 * 2
+		self._rally_skill_data.range_sq = math.pow(1800, 2)
 	end
 end)
 
